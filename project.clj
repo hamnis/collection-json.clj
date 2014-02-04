ba1ff3e1369994571c26cfdee8077a9b9cc58685
Erlend Hamnaberg <erlend@hamnaberg.net>
2014-02-04 19:41:00 +0100

    Remove central, use clojars

--- project.clj
+++ project.clj
@@ -10,10 +10,6 @@
     :developerConnection "git@github.com:hamnis/collection-json.clj.git"
     :connection "git@github.com:hamnis/collection-json.clj.git"
   }
-  :deploy-repositories {
-    "releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/" :creds :gpg}
-    "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/" :creds :gpg}
-  }
   :dependencies [
     [org.clojure/clojure "1.5.1"]
     [net.hamnaberg.rest/json-collection "3.0.0"]
