abe753148097207a974e7e5a307124bcd243e079
Erlend Hamnaberg <erlend@hamnaberg.net>
2014-02-04 19:46:25 +0100

    Bump version after release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.hamnaberg.rest/collection-json-clj "0.1.0"
+(defproject net.hamnaberg.rest/collection-json-clj "0.2.0-SNAPSHOT"
   :description "Wrapper for collection-json java"
   :url "https://github.com/hamnis/collection-json.clj"
   :license {
